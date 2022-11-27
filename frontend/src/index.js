3d1b9487fa97e15a28c578f70c771c7790d26751
kimjisu <jisu3316@naver.com>
2022-11-27 20:45:12 +0900

    라우팅 및 로그인 페이지, jwt 토큰 로컬스토리지 저장, 로그아웃, 회원가입 페이지 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import './index.css';
+import reportWebVitals from "./reportWebVitals";
+import { createRoot } from 'react-dom/client';
+import AppRouter from "./AppRouter";
+// import App from './App';
+
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppRouter tab="home" />);
+
+reportWebVitals();
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App/>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
